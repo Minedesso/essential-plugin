274743bfeb4373ef27843eebc7dc4f67f63a339f
Alessio Lopes <alessio.lopes@adesso.de>
2026-01-08 20:53:19 +0100

    Add .gitignore and integrate Lombok into the project

--- src/main/java/de/minedesso/essentialPlugin/EssentialPlugin.java
+++ src/main/java/de/minedesso/essentialPlugin/EssentialPlugin.java
@@ -1,17 +1,36 @@
 package de.minedesso.essentialPlugin;
 
+import lombok.extern.slf4j.Slf4j;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
+@Slf4j
 public final class EssentialPlugin extends JavaPlugin {
+
+    private final Logger logger = getLogger();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        initializeComponents();
+        logger.info("Essentials plugin has been enabled.");
+    }
 
+    private void initializeComponents() {
+        initializeCommands();
+        initializeListener();
+    }
+
+    private void initializeCommands() {
+    }
+
+    private void initializeListener() {
+        PluginManager pluginManager = getServer().getPluginManager();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Lobby plugin has been disabled.");
     }
 }
