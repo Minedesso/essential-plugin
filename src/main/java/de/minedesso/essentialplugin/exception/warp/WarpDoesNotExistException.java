409048fa130c39f4c0b9b0a7cd8d2ece8801a1d8
Aless1x <101859778+Aless1x@users.noreply.github.com>
2026-01-17 19:54:20 +0100

    Revert "143 implement ec inv command"

--- src/main/java/de/minedesso/essentialplugin/exception/warp/WarpDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package de.minedesso.essentialplugin.exception.warp;
-
-public class WarpDoesNotExistException extends RuntimeException {
-    public WarpDoesNotExistException(String warpName) {
-        super("Warp '" + warpName + "' does not exist.");
-    }
-}
