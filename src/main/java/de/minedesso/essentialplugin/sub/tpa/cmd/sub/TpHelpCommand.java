512f3c05fd611120a132f829a0d3312a2ecf55cb
Fynn Berger <99832883+Lachsfilet@users.noreply.github.com>
2026-01-31 12:55:58 +0100

    Update src/main/java/de/minedesso/essentialplugin/sub/tpa/cmd/sub/TpHelpCommand.java
    Co-authored-by: Aless1x <101859778+Aless1x@users.noreply.github.com>

--- src/main/java/de/minedesso/essentialplugin/sub/tpa/cmd/sub/TpHelpCommand.java
+++ src/main/java/de/minedesso/essentialplugin/sub/tpa/cmd/sub/TpHelpCommand.java
@@ -20,6 +20,10 @@ public class TpHelpCommand implements SubCommand {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (args.length > 0) {
+            sender.sendMessage(Messages.USAGE.message + "/tphelp");
+            return;
+        }
         TpaService.getInstance().displayHelp(sender);
     }
 }
